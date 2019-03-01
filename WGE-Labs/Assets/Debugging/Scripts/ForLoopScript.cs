0628d42320bb52fe20254d3331ede7644a3c3cc2
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2019-03-01 00:09:56 +0000

    Structs Tutorial

---
+++ WGE-Labs/Assets/Debugging/Scripts/ForLoopScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoopScript : MonoBehaviour {
+
+    // start for loop 
+    public void ExecuteLoop()
+    {
+        int x = 0;
+	
+	    System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+	    stopwatch.Start();
+
+        for (int i = 0; i < 500; i++)
+        {
+	        x += i;
+	        //Debug.Log("Value of x = " + x);
+        }
+
+	    stopwatch.Stop();
+	    Debug.Log("Time taken: " + (stopwatch.Elapsed));
+    }
+}
---
+++ WGE-Labs/Assets/Structs Vs Classes/Scripts/StructTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassInts
+{
+    public int x;
+}
+
+public struct StructInts
+{
+    public int x;
+}
+
+
+public class StructTest : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            RunTest();
+        }
+    }
+
+    private void RunTest()
+    {
+        ClassInts[] cInts = new ClassInts[1000000];
+        StructInts[] sInts = new StructInts[1000000];
+
+        for (int i = 0; i < 1000000; ++i)
+        {
+            cInts[i] = new ClassInts();
+        }
+
+        //for loop 1…
+        System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+        stopWatch.Start();
+        for (int i = 0; i < 1000000; i++)
+        {
+            RandomiseClass(cInts[i]);
+        }
+        stopWatch.Stop();
+        System.TimeSpan test1Time = stopWatch.Elapsed;
+
+        stopWatch.Reset();
+        stopWatch.Start();
+        //for loop 2…
+        for (int i = 0; i < 1000000; i++)
+        {
+            RandomiseStruct(ref sInts[i]);
+        }
+        stopWatch.Stop();
+        System.TimeSpan test2Time = stopWatch.Elapsed;
+
+        System.TimeSpan difference = test1Time - test2Time;
+        Debug.Log("Test Times\nTest 1: " + test1Time + "\nTest 2: " + test2Time + "\nDifference: " + difference);
+    }
+
+    void RandomiseStruct(ref StructInts s)
+    {
+        s.x = Random.Range(0, 100);
+    }
+
+    void RandomiseClass(ClassInts c)
+    {
+        c.x = Random.Range(0, 100);
+    }
+}
